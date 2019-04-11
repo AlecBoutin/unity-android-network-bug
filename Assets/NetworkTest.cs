86fa26a8c3f26db93303be94543b96ca838c55d8
Alec Boutin <alec.boutin@gmail.com>
2019-04-11 08:03:04 -0400

    first commit

---
+++ Assets/NetworkTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkTest : MonoBehaviour {
+  public GameObject DoneIndicator;
+
+  // Start is called before the first frame update
+  void Start() {
+    StartCoroutine(Download());
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  IEnumerator Download() {
+    DoneIndicator.SetActive(false);
+    var req = UnityWebRequest.Get("https://files.collective.gg/ugc-cards/p/cards--1837094263.json");
+    var op = req.SendWebRequest();
+    StartCoroutine(LogProgress(req, op));
+    yield return op;
+    Debug.Log("Done");
+    req.Dispose();
+    DoneIndicator.SetActive(true);
+  }
+
+  IEnumerator LogProgress(UnityWebRequest req, UnityWebRequestAsyncOperation op) {
+    while (!op.isDone) {
+      Debug.Log(req.downloadedBytes);
+      yield return null;
+    }
+  }
+}
